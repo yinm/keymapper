1da14e9b40a52270e878e696be2d367365f8194c
yinm <yinm.cminor9@gmail.com>
2018-07-21 15:54:27 +0900

    Add content script

--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,20 @@
-console.log('content-script!')
+import { detectKeyString } from 'key-string'
+import actions from './actions'
+
+const getSettings = () => {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get('settings', ({ settings }) => {
+      resolve(settings)
+    })
+  })
+}
+
+getSettings().then((settings) => {
+  window.addEventListener('keydown', (event) => {
+    const keyString = detectKeyString(event)
+    const actionDefinition = settings.actionDefinitions[keyString]
+    if (actionDefinition) {
+      new actions[actionDefinition.type](actionDefinition).run()
+    }
+  })
+})
