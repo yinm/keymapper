10a9c9d7cac93abb3a1ae53e107ee0664c6a53cf
yinm <yinm.cminor9@gmail.com>
2018-07-21 15:57:29 +0900

    Add background script

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,1 +1,20 @@
-console.log('background !')
+chrome.runtime.onInstalled.addListener(() => {
+  const defaultSettings = {
+    actionDefinitions: {
+      'Ctrl+L': {
+        template: '${title} ${url}',
+        type: 'CopyToClipboard',
+      },
+      'Ctrl+M': {
+        template: '[${title}](${url})',
+        type: 'CopyToClipboard',
+      },
+    },
+  }
+
+  chrome.storage.sync.get('settings', ({ settings }) => {
+    if (!settings) {
+      chrome.storage.sync.set({ settings: defaultSettings })
+    }
+  })
+})
