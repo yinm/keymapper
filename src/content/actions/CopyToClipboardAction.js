afa2f6642ba8d7be9224233a2655ec730527c5e9
yinm <yinm.cminor9@gmail.com>
2018-07-21 16:12:27 +0900

    Add copy to clipboard action

---
+++ src/content/actions/CopyToClipboardAction.js
@@ -0,0 +1,33 @@
+import Action from './Action'
+
+function copyToClipboard(string) {
+  const textarea = document.createelement('textarea')
+  textarea.style.cssText = 'position: absolute; left: -100%;'
+  document.body.appendChild(textarea)
+  textarea.value = string
+  textarea.select()
+  document.execCommand('copy')
+  document.body.removeChild(textarea)
+}
+
+function format(template, variables) {
+  return Object.keys(variables).reduce((result, variableName) => {
+    return result.replace('${' + variableName + '}', variables[variableName])
+  }, template)
+}
+
+export default class CopyToClipboardAction extends Action {
+  static hasValue = true
+
+  run() {
+    copyToClipboard(
+      format(
+        this.value,
+        {
+          title: document.title,
+          url: location.href,
+        }
+      )
+    )
+  }
+}
