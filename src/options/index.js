cf1ea9d658c68b26c9fd6d80d66ac3f2d318f80f
yinm <yinm.cminor9@gmail.com>
2018-07-29 15:24:40 +0900

    Update for using Vue.js

--- src/options/index.js
+++ src/options/index.js
@@ -1,1 +1,10 @@
-console.log('options!')
+import Vue from 'vue'
+import Option from '../components/Option'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  components: { Option },
+  template: '<Option/>',
+})
