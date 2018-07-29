7c3557b891959e0b4b0a9aef533c02a7e1f22fae
yinm <yinm.cminor9@gmail.com>
2018-07-29 14:43:19 +0900

    Setup keymapping

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,20 @@
-console.log('background')
+chrome.runtime.onInstalled.addListener(() => {
+  const defaultSettings = {
+    actionDefinitions: {
+      'Ctrl+L': {
+        value: '${title}\n${url}',
+        type: 'CopyToClipboard',
+      },
+      'Ctrl+M': {
+        value: '[${title}](${url})',
+        type: 'CopyToClipboard',
+      },
+    },
+  }
+
+  chrome.storage.sync.get('settings', ({ settings }) => {
+    if (!settings) {
+      chrome.storage.sync.set({ settings: defaultSettings })
+    }
+  })
+})
