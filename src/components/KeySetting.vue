64e8d43793a317f866bd2193020f4997c6eae9b9
yinm <yinm.cminor9@gmail.com>
2018-07-29 16:10:36 +0900

    Add key settings

---
+++ src/components/KeySetting.vue
@@ -0,0 +1,35 @@
+<template>
+  <tr>
+    <td>
+      <kbd>{{ keyString }}</kbd>
+    </td>
+    <td>
+      {{ actionDefinitions.type }}
+    </td>
+    <td>
+      {{ actionDefinitions.value }}
+    </td>
+    <td>
+      <button @click="onDelete">x</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+  export default {
+    name: 'KeySetting',
+    props: {
+      keyString: String,
+      actionDefinitions: Object,
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+      onDelete() {
+        this.$emit('on-delete', this.keyString)
+      }
+    },
+  }
+</script>
--- src/components/Option.vue
+++ src/components/Option.vue
@@ -1,17 +1,42 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
-    <h2>testttt</h2>
+    <table>
+      <tbody>
+        <key-setting
+          v-for="keyString in Object.keys(settings.actionDefinitions)"
+          :key="keyString"
+          :key-string="keyString"
+          :action-definitions="settings.actionDefinitions[keyString]"
+          @on-delete="onDelete"
+        />
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+  import KeySetting from './KeySetting'
+
   export default {
     name: 'option',
+    components: {KeySetting},
     data() {
       return {
-        message: 'Option!!!'
+        settings: {
+          actionDefinitions: {},
+        },
       }
     },
+    mounted() {
+      chrome.storage.sync.get('settings', ({ settings }) => {
+        this.settings = settings
+      })
+    },
+    methods: {
+      onDelete(keyString) {
+        this.$delete(this.settings.actionDefinitions, keyString)
+        chrome.storage.sync.set({ settings: this.settings })
+      },
+    }
   }
 </script>
