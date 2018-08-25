42f8e6d2379e17acc791ec0973bf40f017921667
yinm <yinm.cminor9@gmail.com>
2018-08-25 09:12:37 +0900

    Add scrollToTopAction

---
+++ src/content/actions/ScrollToTopAction.js
@@ -0,0 +1,19 @@
+import Action from './Action'
+
+export default class ScrollToTopAction extends Action {
+  static hasValue() {
+    return false
+  }
+
+  run() {
+    scrollToTop()
+  }
+}
+
+function scrollToTop() {
+  const currentY = window.pageYOffset
+  window.scrollTo(0, Math.floor(currentY / 2))
+  if (currentY > 0) {
+    window.setTimeout(scrollToTop, 20)
+  }
+}
