90faaef1d4271475e16e37a62b8dfe851551a2e2
yinm <yinm.cminor9@gmail.com>
2018-08-25 09:12:37 +0900

    Add scrollToTopAction

--- src/content/actions/index.js
+++ src/content/actions/index.js
@@ -1,11 +1,13 @@
 import CopyToClipboardAction from './CopyToClipboardAction'
 import ScrollDownAction from './ScrollDownAction'
 import ScrollUpAction from './ScrollUpAction'
+import ScrollToTop from './ScrollToTopAction'
 
 const actions = {
   CopyToClipboard: CopyToClipboardAction,
   ScrollUpAction: ScrollUpAction,
   ScrollDownAction: ScrollDownAction,
+  ScrollToTop: ScrollToTop,
 }
 
 export default actions
