e9193eecf1ef967be2379cd763e485610b8cd440
yinm <yinm.cminor9@gmail.com>
2018-08-25 10:01:37 +0900

    Rename for more readable code

--- src/content/actions/ScrollToBottomAction.js
+++ src/content/actions/ScrollToBottomAction.js
@@ -12,6 +12,6 @@ export default class ScrollToBottomAction extends Action {
 
 function scrollToBottom() {
   const rootElement = document.documentElement
-  const y = rootElement.scrollHeight - rootElement.clientHeight
-  window.scroll(0, y)
+  const bottomY = rootElement.scrollHeight - rootElement.clientHeight
+  window.scroll(0, bottomY)
 }
