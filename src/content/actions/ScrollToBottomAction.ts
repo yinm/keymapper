4cfb1b8ed80a69274cfc5a2e11f86699f0e13422
yinm <yinm.cminor9@gmail.com>
2018-09-21 21:40:35 +0900

    Defined before it was used

--- src/content/actions/ScrollToBottomAction.ts
+++ src/content/actions/ScrollToBottomAction.ts
@@ -1,15 +1,15 @@
 import Action from './Action'
 
+function scrollToBottom() {
+  const rootElement = document.documentElement
+  const bottomY = rootElement.scrollHeight - rootElement.clientHeight
+  window.scroll(0, bottomY)
+}
+
 export default class ScrollToBottomAction extends Action {
   public static hasValue: boolean = false
 
   public run() {
     scrollToBottom()
   }
-}
-
-function scrollToBottom() {
-  const rootElement = document.documentElement
-  const bottomY = rootElement.scrollHeight - rootElement.clientHeight
-  window.scroll(0, bottomY)
 }
