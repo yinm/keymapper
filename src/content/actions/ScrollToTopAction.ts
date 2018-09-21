8259e9b5db621b26fd91a9ee46ad712992cf5896
yinm <yinm.cminor9@gmail.com>
2018-09-21 21:41:48 +0900

    Defined before it was used

--- src/content/actions/ScrollToTopAction.ts
+++ src/content/actions/ScrollToTopAction.ts
@@ -1,17 +1,17 @@
 import Action from './Action'
 
-export default class ScrollToTopAction extends Action {
-  public static hasValue: boolean = false
-
-  public run() {
-    scrollToTop()
-  }
-}
-
 function scrollToTop() {
   const currentY = window.pageYOffset
   window.scrollTo(0, Math.floor(currentY / 2))
   if (currentY > 0) {
     window.setTimeout(scrollToTop, 20)
+  }
+}
+
+export default class ScrollToTopAction extends Action {
+  public static hasValue: boolean = false
+
+  public run() {
+    scrollToTop()
   }
 }
