7a2e7ef7f7f84079b9feaf1ac0160cc24f357ecc
yinm <yinm.cminor9@gmail.com>
2023-12-23 19:25:14 +0900

    Add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import { Header } from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="bg-[#f8f9fa]">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
