203943d92ab8ab37f2b734a0c157696ae41542c3
yinm <yinm.cminor9@gmail.com>
2023-12-23 20:03:27 +0900

    Add main area styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="bg-[#f8f9fa]">
+    <div className="h-screen bg-[#f8f9fa]">
       <Header />
+      <main className="mx-auto max-w-5xl">
+        <table className="mt-8 flex w-full rounded-lg bg-white p-8">
+          <tbody className="flex-auto divide-y divide-solid divide-[#ddd]"></tbody>
+        </table>
+      </main>
     </div>
   );
 }
